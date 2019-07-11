20f9b0b91adaeac3680e0450052a99b0ec4139f4
sychovSaveliy <sychov.saveliy@gmail.com>
2019-07-11 20:35:00 +0200

    L-10

---
+++ lections/lection_10_this+practice/materials/main.js
@@ -0,0 +1,81 @@
+// var foo = 1; 
+// function bar() { 
+//     if (!foo) { 
+//          foo = 10; 
+//     } 
+//     alert(foo); 
+// } 
+// bar();
+
+
+
+// -- Замыкание ----
+
+function parent() {
+    var count = 0;
+    //LE_parent = {count: 0, scope: window}
+    return function () {
+        //LE_child = {scope: LE_parent}
+        return count += 1;
+    }
+}
+
+
+var child = parent();
+
+parent = null;
+
+console.log(child(), 'child');
+console.log(child(), 'child');
+console.log(child(), 'child');
+
+// ---- this - контекст вызова ф-ии ------------
+var talk = "window say ...";
+
+var human = {
+    name: "Valera",
+    talk: "Bla bla bla..",
+    say: say
+}
+
+var cat = {
+    name: 'Мурзик',
+    talk: "Mяу, Мяу, мяу ...",
+    say: say
+}
+
+function say() {
+    console.log('say something ' + this.talk);
+}
+
+human.say()
+cat.say()
+
+
+say(); /// window.say()
+
+
+var math = {
+    x: 10,
+    y: 10,
+    sum: function() {
+        return this.x + this.y
+    }
+}
+
+console.log(math.sum())
+
+var mass = [1,2,3,4,5];
+
+console.log(mass, 'mass')
+
+mass.myPush = function(value) {
+    console.log(this, 'this');
+    this[this.length] = value + "  !  ";
+}
+
+mass.myPush('123123');
+
+console.log(mass, 'mass~!!')
+
+\
