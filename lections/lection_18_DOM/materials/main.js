04fb3c51ff65feff86d3f0ead20fba4e50e16daf
ValeriyTernavskiy <valeraternavckiy1994@gmail.com>
2019-08-08 18:17:55 +0300

    L-18 + UPD

---
+++ lections/lection_18_DOM/materials/main.js
@@ -0,0 +1,20 @@
+/*
+    /*
+   1. attrs: getAttribute, setAttribute, removeAttribute,
+    dataset,
+   2. cycles event
+   3. Навесить событие на дин-е меню.: for, target 
+*/
+
+
+
+
+window.onload = function() {
+  
+    
+
+} 
+
+
+
+
