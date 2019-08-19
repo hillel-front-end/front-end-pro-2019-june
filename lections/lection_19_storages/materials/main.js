6f453a1184e889817a3e61b7cbdf77465b968876
MrTernavskiy <valerater@ukr.net>
2019-08-19 20:40:40 +0300

    l-19

--- lections/lection_19_storages/materials/main.js
+++ lections/lection_19_storages/materials/main.js
@@ -16,6 +16,38 @@ JSON поддерживает следующие типы данных:
 
 window.onload = function() {
 
+    console.log(localStorage["foo"]);
+
+    console.log(localStorage.getItem('foo'));
+
+
+    localStorage.setItem('foo', [123,6,7,7]);
+
+    console.log(typeof localStorage.getItem('foo'))
+
+    document.body.append(localStorage.getItem('foo') || '');
+
+
+    var obj = {
+        name: 'Valera',
+        age: 123,
+        isMarried: true,
+        foo: null
+    }
+
+
+    var str = JSON.stringify(obj);
+
+    localStorage.setItem('foo', str);
+    console.log(str, 'str');
+
+
+    var foo = JSON.parse(localStorage.getItem('foo'));
+
+    console.log(foo, 'foo');
+
+
+
 }
 
 
